86ff9d19fd4d813bbe306be75c6cc880c4e5525b
thachacon <thachacon@gmail.com>
2024-07-02 18:29:09 -0300

    last commit

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -15,9 +15,13 @@ public class ContaTerminal {
         System.out.println("Digite o seu sobrenome:\n");
         String sobrenome = texto.next();
 
-        System.out.println("Digite o número da Agência. Exemplo: 1021");
+        System.out.println("Digite o número da Agência. Exemplo: 1021\n");
         int conta = texto.nextInt();
 
+        String numeroConta = "067-8";
         double saldoConta = 237.48;
+
+        System.out.println("Olá, obrigado " + nome + " " + sobrenome + " por criar uma conta em nosso banco, sua agência é " + conta+ ", conta de número " + numeroConta + " e seu saldo " + saldoConta + " já está disponível para saque.");
+
     }
 }
