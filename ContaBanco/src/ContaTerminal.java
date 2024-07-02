8de3d895a3d8ca1eeca3f5db1b1dc4a6ab41f645
thachacon <thachacon@gmail.com>
2024-07-02 18:31:08 -0300

    last commit

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -3,13 +3,10 @@ import java.util.Scanner;
 public class ContaTerminal {
     
     public static void main(String[] args) {
-        //TO DO: conhecer e importar a classe Scanner
-        // exibir as mensagens para o nosso usuário
-        // obter pela classe scanner os valores digitados no terminal 
-        // exibir a mensagem conta criada
+
         Scanner texto = new Scanner(System.in);
 
-        System.out.println("Olá! Seja bem-vindo (a) ao banco Dio! Digite o seu nome:\n");
+        System.out.println("Olá! Seja bem-vindo (a) ao banco XXX! Digite o seu nome:\n");
         String nome = texto.next();
 
         System.out.println("Digite o seu sobrenome:\n");
@@ -21,7 +18,10 @@ public class ContaTerminal {
         String numeroConta = "067-8";
         double saldoConta = 237.48;
 
-        System.out.println("Olá, obrigado " + nome + " " + sobrenome + " por criar uma conta em nosso banco, sua agência é " + conta+ ", conta de número " + numeroConta + " e seu saldo " + saldoConta + " já está disponível para saque.");
+        System.out.println("Olá, obrigado " + nome + " " + sobrenome + 
+        " por criar uma conta em nosso banco, sua agência é " + conta+ 
+        ", conta de número " + numeroConta + " e seu saldo " + saldoConta + 
+        " já está disponível para saque.");
 
     }
 }
