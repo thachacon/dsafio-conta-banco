582ec7ddd32f06e97b1c710f09dfd0f817d39841
thachacon <thachacon@gmail.com>
2024-07-02 18:12:04 -0300

    first commit

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        //TO DO: conhecer e importar a classe Scanner
+        // exibir as mensagens para o nosso usuário
+        // obter pela classe scanner os valores digitados no terminal 
+        // exibir a mensagem conta criada
+        Scanner texto = new Scanner(System.in);
+
+        System.out.println("Olá! Seja bem-vindo (a) ao banco Dio! Digite o seu nome:\n");
+        String nome = texto.next();
+
+        System.out.println("Digite o seu sobrenome:\n");
+        String sobrenome = texto.next();
+
+        System.out.println("Digite o número da Agência. Exemplo: 1021");
+        int conta = texto.nextInt();
+
+        double saldoConta = 237.48;
     }
 }
